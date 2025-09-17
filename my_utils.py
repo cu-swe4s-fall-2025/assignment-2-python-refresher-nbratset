bab798d4817e74f7f50f545846ab4829230e6478
nbratset <nbratset@gmail.com>
2025-09-16 23:49:39 -0600

    Reworked get_column to handle some errors and commandline arguments

--- my_utils.py
+++ my_utils.py
@@ -5,9 +5,9 @@ def get_column(file_name, query_column, query_value, result_column=1):
     ----------
     file_name : csv file name
 
-    query_column : Column index to filter by
+    query_column : Column index to filter by (can be a string or int)
 
-    query_value : Value within specified query_column to filter by
+    query_value : Value within specified query_column to filter by (can be a string or int)
 
     result_column : Column index or named column to get result values from
 
@@ -16,19 +16,33 @@ def get_column(file_name, query_column, query_value, result_column=1):
     result_array :  List of values from result_column where query_column contains the query_value
     
     '''
+    try:
+        f = open(file_name, 'r')
+    except FileNotFoundError:
+        print('Could not find '+file_name)
+    except PermissionError:
+        print('Could not open '+file_name)
     
-    f = open(file_name, 'r')
+    column_header = f.readline()                             # reads first line to get column headers
     
-    if type(result_column) == str:                         # checks if result_column is a string
-        column_header = f.readline()                       # reads first line to get column headers
-        result_index = column_header.index(result_column)  # finds column index for the result_column value
-    elif type(result_column) == int:
-        result_index = result_column
+    try:
+        query_index=int(query_column)
+    except ValueError:
+        query_index = column_header.index(query_column)      # finds column index for the query_column string value
     
-    result_array = []                                      # Final result list to be appended to
-    for line in f:                                         # iterrates over all lines in csv
-        line_array = line.rstrip().split(',')              # splits line into an array
-        if line_array[query_column] == query_value:        # checks for query_value in the query_column
-            result_array.append(line_array[result_index]) # appends value if conditions are met to an array
+    try:                                                     # checks if result_column is a string or int
+        result_index=int(result_column)
+    except ValueError:
+        result_index = column_header.index(result_column)    # finds column index for the result_column string value
+                                                                                                        
+    result_array = []                                        # Final result list to be appended to
+
+    for line in f:                                           # iterrates over all lines in csv
+        line_array = line.rstrip().split(',')                # splits line into an array
+        
+        if line_array[query_index] == query_value:           # checks for query_value in the query_column
+            result_array.append(line_array[result_index])    # appends value if conditions are met to an array
+    
     f.close()
+
     return result_array
