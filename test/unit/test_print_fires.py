bdf576eb4541dc43aef76e2f54db111eede24b5e
nbratset <nbratset@gmail.com>
2025-09-30 17:42:06 -0600

    fixed median random array test and un-commented rest of median testing

--- test/unit/test_print_fires.py
+++ test/unit/test_print_fires.py
@@ -30,26 +30,21 @@ class TestMathLib(unittest.TestCase):
         self.assertEqual(my_utils.median([1, 2, 3, 4, 5]), 3) # odd case
         self.assertEqual(my_utils.median([2, 5, 6, 9, 12, 7]), np.median([2, 5, 6, 9, 12, 7])) #out of order list
 
-    # def test_median_errors(self):
-    #     with self.assertRaises(SystemExit) as cm:
-    #         my_utils.median([])
-    #     self.assertEqual(cm.exception.code, 1)
+    def test_median_errors(self):
+        self.assertEqual(my_utils.mean([]), None)
+        self.assertEqual(my_utils.mean(['1', '2', '3']), 2.0)
 
-    #     with self.assertRaises(SystemExit) as cm:
-    #         my_utils.median('string')
-    #     self.assertEqual(cm.exception.code, 1)
+    def test_random_median(self):
+        for i in range(0,10):
+            length = random.randint(1, 100)
+            random_array = [random.randint(1, 99) for _ in range(length)]
+            self.assertEqual(my_utils.median(random_array), np.median(random_array))
 
-    # def test_random_median(self):
-    #     for i in range(0,10):
-    #         length = random.randint(1, 100)
-    #         random_array = random.randrange(0,100,length)
-    #         self.assertEqual(my_utils.median(random_array, np.median(random_array)))
-
-    # def test_stdev(self):
-    #     self.assertEqual(my_utils.stdev(10, 5), 50)
-    #     self.assertEqual(my_utils.stdev(-1, 1), -1)
-    #     self.assertIsNone(my_utils.stdev([10]))
-    #     self.assertEqual(my_utils.stdev([10, 1, 20, 13, 5, 12, 11]), np.std([10, 1, 20, 13, 5, 12, 11]))
+    def test_stdev(self):
+        self.assertEqual(my_utils.stdev([10, 5]), np.std([10, 5]))
+        self.assertEqual(my_utils.stdev([-1, 1]), np.std([-1, 1]))
+        self.assertEqual(my_utils.stdev([10]), 0.0)
+        self.assertEqual(my_utils.stdev([10, 1, 20, 13, 5, 12, 11]), np.std([10, 1, 20, 13, 5, 12, 11]))
     
     # def test_random_stdev(self):
     #     for i in range(0,10):
