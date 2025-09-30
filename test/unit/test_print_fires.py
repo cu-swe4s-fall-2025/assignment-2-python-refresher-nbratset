3e5ab8cc67ca3a7695e92c6a464ecbc05e117423
nbratset <nbratset@gmail.com>
2025-09-29 23:08:41 -0600

    Added random tests

--- test/unit/test_print_fires.py
+++ test/unit/test_print_fires.py
@@ -15,19 +15,34 @@ class TestMathLib(unittest.TestCase):
         self.assertEqual(my_utils.mean([2, 5, 6, 9, 12, 7]), np.mean([2, 5, 6, 9, 12, 7]))
         self.assertEqual(my_utils.mean([10]), 10)
 
-    # def test_mean_errors(self):
-    #     pass
+    def test_random_mean(self):
+        for i in range(0,10):
+            length = random.randint(1, 100)
+            random_array = random.randrange(0,100,length)
+            self.assertEqual(my_utils.mean(random_array, np.mean(random_array)))
 
     def test_median(self):
         self.assertEqual(my_utils.median([1, 2, 3, 4]), 2.5)  # even case
         self.assertEqual(my_utils.median([1, 2, 3, 4, 5]), 3) # odd case
         self.assertEqual(my_utils.median([2, 5, 6, 9, 12, 7]), np.median([2, 5, 6, 9, 12, 7])) #out of order list
 
+    def test_random_median(self):
+        for i in range(0,10):
+            length = random.randint(1, 100)
+            random_array = random.randrange(0,100,length)
+            self.assertEqual(my_utils.median(random_array, np.median(random_array)))
+
     def test_stdev(self):
         self.assertEqual(my_utils.stdev(10, 5), 50)
         self.assertEqual(my_utils.stdev(-1, 1), -1)
         self.assertIsNone(my_utils.stdev([10]))
         self.assertEqual(my_utils.stdev([10, 1, 20, 13, 5, 12, 11]), np.std([10, 1, 20, 13, 5, 12, 11]))
+    
+    def test_random_stdev(self):
+        for i in range(0,10):
+            length = random.randint(1, 100)
+            random_array = random.randrange(0,100,length)
+            self.assertEqual(my_utils.stdev(random_array, np.std(random_array)))
 
 
 if __name__ == '__main__':
