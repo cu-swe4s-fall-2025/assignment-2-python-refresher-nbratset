3eea2178b5c78ba7a381af50d27c0304f73bc222
nbratset <nbratset@gmail.com>
2025-09-30 23:15:14 -0600

    updated style using pycodestyle to follow pep8

--- src/my_utils.py
+++ src/my_utils.py
@@ -1,22 +1,26 @@
-import sys 
+import sys
 
-def get_column(file_name, query_column, query_value, result_column=1):
-    '''Reads a CSV file and finds lines with a given value in a given column. Returns value of speficied column as an array.
-    
+
+def get_column(file_name, query_column,
+               query_value, result_column=1):
+    '''Reads a CSV file and finds lines with a given value in a given column.
+    Returns value of speficied column as an array.
+
     Parameters
     ----------
     file_name : csv file name
 
     query_column : Column index to filter by (can be a string or int)
 
-    query_value : Value within specified query_column to filter by (can be a string or int)
+    query_value : Value within specified query_column to filter by
+                (can be a string or int)
 
     result_column : Column index or named column to get result values from
 
-    Returns 
+    Returns
     -------
-    result_array :  List of values from result_column where query_column contains the query_value
-    
+    result_array :  List of values from result_column
+                    where query_column contains the query_value
     '''
     try:
         f = open(file_name, 'r')
@@ -24,51 +28,61 @@ def get_column(file_name, query_column, query_value, result_column=1):
         print(f'Could not find file: {file_name}')
     except PermissionError:
         print(f'Could not open file: {file_name}')
-    
-    column_header = f.readline()                          # reads first line to get column headers
-    
+
+    column_header = f.readline()  # reads first line to get column headers
+
     try:
-        query_index=int(query_column)
+        query_index = int(query_column)
     except ValueError:
-        query_index = column_header.index(query_column)       # finds column index for the query_column string value
-    
-    try:                                                      # checks if result_column is a string or int
-        result_index=int(result_column)
+        # finds column index for the query_column string value
+        query_index = column_header.index(query_column)
+
+    try:  # checks if result_column is a string or int
+        result_index = int(result_column)
     except ValueError:
         try:
             result_column in column_header
-            result_index = column_header.index(result_column) # finds column index for the result_column string value
+            # finds column index for the result_column string value
+            result_index = column_header.index(result_column)
         except ValueError:
             print(f'{result_column} column not in {file_name}')
-                                                                                                        
-    result_array = []                                         # Final result list to be appended to
 
-    for line in f:                                            # iterrates over all lines in csv
-        line_array = line.rstrip().split(',')                 # splits line into an array
-        
-        if line_array[query_index] == query_value:            # checks for query_value in the query_column
+    # Final result list to be appended to
+    result_array = []
+
+    # iterrates over all lines in csv
+    for line in f:
+        line_array = line.rstrip().split(',')  # splits line into an array
+
+        # checks for query_value in the query_column
+        if line_array[query_index] == query_value:
             try:
-                value=float(line_array[result_index])         # Saves value as a float
-                result_array.append(int(value))               # appends value as an integer to an array (since the instructions wanted integers)
+                # Saves value as a float
+                value = float(line_array[result_index])
+                # appends value as an integer to an array
+                # (since the instructions wanted integers)
+                result_array.append(int(value))
             except IndexError:
                 print(f'{result_column} column not in {file_name}')
-    
+
     f.close()
 
     return result_array
 
+
 def mean(array):
-    ''' Calculates mean via (sum of all values in array) / (number of values in array). '''
+    ''' Calculates mean via:
+    (sum of all values in array) / (number of values in array). '''
     try:
         total = len(array)
     except IndexError:
         print('Array provided was empty!')
         return None
-    
+
     if total == 0:
         print('Array provided was empty!')
         return None
-    
+
     sum = 0.0
     for value in array:
         try:
@@ -76,18 +90,21 @@ def mean(array):
         except (ValueError, TypeError):
             print('Array contains non-number characters!')
             return None
-    
+
     return sum / total
 
+
 def median(array):
-    ''' Calculates median for both odd and even number arrays by finding the middle index of the sorted list. 
-        For an even array, the funtion finds the middle two indexes and returns the average of the two. '''
-    try: 
+    ''' Calculates median for both odd and even number arrays
+        by finding the middle index of the sorted list.
+        For an even array, the funtion finds the middle two
+        indexes and returns the average of the two. '''
+    try:
         float_array = [float(x) for x in array]
     except (ValueError, TypeError):
-            print('Array contains non-number characters!')
-            return None
-    
+        print('Array contains non-number characters!')
+        return None
+
     try:
         length = len(float_array)
     except IndexError:
@@ -103,34 +120,36 @@ def median(array):
     else:
         return sorted_array[median_index]
 
+
 def stdev(array):
     ''' Calculates the standard deviation of an array via:
-        Sqrt(Summation of (x-mean)^2 / N) where N is the number of elements in the array. '''
+        Sqrt(Summation of (x-mean)^2 / N)
+        where N is the number of elements in the array. '''
     array_mean = mean(array)
-    
+
     if array_mean is None:
         print('Array provided was empty!')
         return None
-    
+
     try:
         length = len(array)
     except IndexError:
         print('Array provided was empty!')
         return None
-    
+
     if length <= 1:
         print('Cannot compute stdev of an array with less than 2 elements.')
         return 0.0
-    
-    try: 
+
+    try:
         float_array = [float(x) for x in array]
     except (ValueError, TypeError):
-            print('Array contains non-number characters!')
-            return None
+        print('Array contains non-number characters!')
+        return None
 
     sum = 0.0
     for value in float_array:
         delta_x_squared = (value - array_mean) ** 2
         sum += delta_x_squared
-    
+
     return (sum / length) ** 0.5
