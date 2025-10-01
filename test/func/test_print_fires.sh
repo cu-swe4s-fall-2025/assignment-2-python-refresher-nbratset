47275d40d7436a4c6c733afa1d2a782ccb93530a
nbratset <nbratset@gmail.com>
2025-09-30 22:23:01 -0600

    Added functional tests

---
+++ test/func/test_print_fires.sh
@@ -0,0 +1,49 @@
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+. ssshtest
+
+## Positive cases ##
+run test_mean_by_name python src/print_fires.py -f 'test_data.csv' -c 'United States of America' -cc 'Area' -fc 'Forest fires' -o 'mean'
+assert_in_stdout "1999.5617"
+assert_in_stdout "Mean: 1928.79919"
+assert_exit_code 0
+
+run test_median_by_index python src/print_fires.py -f 'test_data.csv' -c 'United States of America' -cc 'Area' -fc 'Forest fires' -o 'median'
+assert_in_stdout "1999.5617"
+assert_in_stdout "Median: 1558.7559"
+assert_exit_code 0
+
+run test_stdev python src/print_fires.py -f 'test_data.csv' -c 'United States of America' -cc 'Area' -fc 'Forest fires' -o 'stdev'
+assert_in_stdout "55.3751"
+assert_in_stdout "Standard Deviation: 72.65564"
+assert_exit_code 0
+
+run test_all python src/print_fires.py -f 'test_data.csv' -c 'United States of America' -cc 'Area' -fc 'Forest fires' -o 'all'
+assert_in_stdout "Mean: 200.0"
+assert_in_stdout "Median: 200.0"
+assert_in_stdout "Standard Deviation: 81.64965809277261"
+assert_exit_code 0
+
+
+## Error Cases ##
+run test_file_not_found python src/print_fires.py -f 'file.csv' -c 'Any' -cc 'Any' -fc 'Any'
+assert_in_stderr "Could not find file: file.csv"
+assert_exit_code 0
+
+run test_bad_query_column python src/print_fires.py -f 'test_data.csv' -c 'USA' -cc 'bad_col' -fc 'Forest fires'
+assert_in_stderr "substring not found"
+assert_exit_code 1
+
+run test_bad_result_column python src/print_fires.py -f 'test_data.csv' -c 'United States of America' -cc 'Area' -fc 'Bad_Fires'
+assert_in_stderr "Bad_Fires column not in test_data.csv"
+assert_exit_code 0
+
+run test_empty_array python src/print_fires.py -f 'test_data.csv' -c 'Not a Country' -cc 'Area' -fc 'Forest fires' -o 'mean'
+assert_in_stdout "[]"
+assert_in_stdout "Array provided was empty!"
+assert_in_stdout "Mean: None"
+assert_exit_code 0
+
+run test_wrong_operator src/print_fires.py -f 'test_data.csv' -c 'United States of America' -cc 'Area' -fc 'Forest fires' -o 'mode'
+assert_in_stdout "1999.5617"
+assert_not_in_stdout "Mean"
+assert_exit_code 0
